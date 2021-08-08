145a440e89dd4ff14f1a964bf3e7ba6fc807c1e5
my powerful PC <peijin.he@qq.com>
2021-08-08 21:31:36 +1200

    demo file

---
+++ SocketClient/SocketClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketClient client = new SocketClient(8888);
+            client.StartClient();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SocketServer/SocketServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using SocketUtil;
+
+namespace SocketUtil
+{
+    class Program
+    {
+        public  static Socket socketServer = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+        private static byte[] buffer = new byte[1024 * 1024 * 2];
+        static void Main(string[] args)
+        {
+            SocketServer server = new SocketServer(8888);
+            server.StartListen();
+            Console.ReadKey();
+        }
+
+    }
+}
